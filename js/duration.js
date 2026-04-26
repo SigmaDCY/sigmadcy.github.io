a1efdad790191a3209d80487f6917fb5395a53ef
SigmaDCY <dingdang77_2024@outlook.com>
2026-04-26 15:30:58 +0800

    Site updated: 2026-04-26 15:30:57

--- js/duration.js
+++ js/duration.js
@@ -1,6 +1,6 @@
 !(function() {
   /** 计时起始时间，自行修改 **/
-  var start = new Date("2025/06/13 19:31:47");
+  var start = new Date("2026/04/24 21:30:00");
 
   function update() {
     var now = new Date();
@@ -25,6 +25,7 @@
 	/** 显示的文字内容，详细你结合实际渲染结果应该能看出改哪里 **/
     document.getElementById("timeDate").innerHTML = "本站已在随时准备跑路的状态下运行了&nbsp"+dnum+"&nbsp天";
     document.getElementById("times").innerHTML = hnum + "&nbsp小时&nbsp" + mnum + "&nbsp分&nbsp" + snum + "&nbsp秒";
+    document.getElementById("miracle").innerHTML = "，实在是一个奇迹";
   }
 
   update();
